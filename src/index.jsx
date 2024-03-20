98cb9f242f1ec71c57beddc92473e6727b267b0f
SideeID <sandipratama164026@gmail.com>
2024-03-20 15:40:45 +0700

    setup

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello, React!</h1>
+    }
+}
+
+const root = createRoot(document.getElementById("root"))
+root.render(<App />)
