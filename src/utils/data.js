7fc2a35bd20a73ceb31809bf47cf92eadaadd173
SideeID <sandipratama164026@gmail.com>
2024-03-21 13:46:32 +0700

    Add data.js file with initial data

---
+++ src/utils/data.js
@@ -0,0 +1,41 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            title: 'mandi',
+            body: 'mandi di jam 5 pagi',
+            archived: false,
+            createAt: new Date()
+        },
+        {
+            id: 2,
+            title: 'belanja',
+            body: 'belanja di pasar',
+            archived: false,
+            createAt: new Date()
+        },
+        {
+            id: 3,
+            title: 'olahraga',
+            body: 'olahraga di lapangan',
+            archived: false,
+            createAt: new Date()
+        },
+        {
+            id: 4,
+            title: 'makan',
+            body: 'makan di rumah',
+            archived: false,
+            createAt: new Date()
+        },
+        {
+            id: 5,
+            title: 'tidur',
+            body: 'tidur di jam 9 malam',
+            archived: false,
+            createAt: new Date()
+        },
+    ];
+};
+
+export { getData };
