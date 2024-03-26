177f8b73a7c201d70b3809923ffccc689743dbe3
SideeID <sandipratama164026@gmail.com>
2024-03-26 16:04:27 +0700

    Add onUnarchive prop to ArchiveButton component

--- src/components/common/ArchiveButton.jsx
+++ src/components/common/ArchiveButton.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function ArchiveButton({ onArchive, children, archived }) {
+function ArchiveButton({ onArchive, children, archived, onUnarchive }) {
     const buttonText = archived ? "Restore" : children;
 
     return (
-        <button onClick={onArchive} className="note-item__archive">
+        <button onClick={archived ? onUnarchive : onArchive} className="note-item__archive">
             {buttonText}
         </button>
     );
 }
+
+ArchiveButton.propTypes = {
+    onArchive: PropTypes.func.isRequired,
+    onUnarchive: PropTypes.func.isRequired,
+    archived: PropTypes.bool.isRequired,
+    children: PropTypes.string.isRequired
+};
 
 export default ArchiveButton;
