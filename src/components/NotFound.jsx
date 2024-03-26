899b0c82edb64d2c32c85802d926d4b99ccd5c85
SideeID <sandipratama164026@gmail.com>
2024-03-26 16:06:19 +0700

    Add prop-types, react-icons, and react-router-dom dependencies

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFoundPage() {
+    return (
+        <div className="not-found-page">
+            <h1>404 - Halaman Tidak Ditemukan</h1>
+            <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
+        </div>
+    );
+}
+
+export default NotFoundPage;
