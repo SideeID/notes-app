a97556e1117b2664b3a1be68d5df9f0b0b626399
SideeID <sandipratama164026@gmail.com>
2024-03-26 16:03:27 +0700

    Add createdAt prop to NoteItemBody component

--- src/components/NoteItemBody.jsx
+++ src/components/NoteItemBody.jsx
@@ -1,12 +1,20 @@
-import React from "react"
+import React from "react";
+import PropTypes from 'prop-types';
 
-function NoteItemBody({ title, body }) {
+function NoteItemBody({ title, body, createdAt }) {
     return (
         <div className="note-item__body">
             <h3 className="note-item__title">{title}</h3>
+            <p className="note-item__createdAt">Dibuat: {createdAt}</p>
             <p className="note-item__body">{body}</p>
         </div>
-    )
+    );
 }
 
-export default NoteItemBody
+NoteItemBody.propTypes = {
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired
+};
+
+export default NoteItemBody;
