6eb47dd50b45fadc1ef790f34ab8d2b531bd341f
SideeID <sandipratama164026@gmail.com>
2024-03-26 16:03:06 +0700

    Add PropTypes and Link to NoteItem component

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -2,19 +2,34 @@ import React from "react";
 import NoteItemBody from "./NoteItemBody";
 import DeleteButton from "./common/DeleteButton";
 import ArchiveButton from "./common/ArchiveButton";
+import { showFormattedDate } from "../utils"; 
+import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
-function NoteItem({ title, body, id, archived, onDelete, onArchive }) {
+function NoteItem({ title, body, createdAt, id, archived, onDelete, onArchive, onUnarchive }) {
     const archiveButtonText = archived ? "Restore" : "Archive";
 
     return (
         <div className={`note-item ${archived ? 'archived' : ''}`}>
-            <NoteItemBody title={title} body={body} />
+            <Link to={`/note/${id}`} className="link-no-decoration">
+                <NoteItemBody title={title} body={body} createdAt={showFormattedDate(createdAt)} />
+            </Link>
             <div className="button-container">
-                <DeleteButton onDelete={() => onDelete(id)} />
-                <ArchiveButton onArchive={() => onArchive(id)}>{archiveButtonText}</ArchiveButton>
+                <DeleteButton onDelete={() => onDelete(id)} id={id} />
+                <ArchiveButton onArchive={() => onArchive(id) } onUnarchive={() => onUnarchive(id)} archived={archived}>{archiveButtonText}</ArchiveButton>
             </div>
         </div>
     );
 }
 
-export default NoteItem
+NoteItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    archived: PropTypes.bool.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onArchive: PropTypes.func.isRequired
+};
+
+export default NoteItem;
