a054e33e3b7ad5a71b3ed5509871c152b443c87a
SideeID <sandipratama164026@gmail.com>
2024-03-26 16:05:13 +0700

    Refactor index.jsx to use BrowserRouter for routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import NoteApp from "./components/NoteApp"
+import { BrowserRouter } from "react-router-dom"
 import "./styles/style.css"
 
-const root = createRoot(document.getElementById("root"))
-root.render(<NoteApp />)
+createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <NoteApp />
+    </BrowserRouter>
+)
