e3145b8588f38514888d7b3602f7dd7d870134f4
SideeID <sandipratama164026@gmail.com>
2024-03-26 16:03:47 +0700

    Add onUnarchive prop to NoteList component

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import NoteItem from "./NoteItem";
+import PropTypes from 'prop-types';
 
-function NoteList({ notes, onDelete, onArchive }) {
+function NoteList({ notes, onDelete, onArchive, onUnarchive }) {
     if (notes.length === 0) {
-        return <p className="note-list__empty">Tidak ada catatan</p>
+        return <p className="note-list__empty">Tidak ada catatan</p>;
     }
 
     return (
         <div className="note-list">
             {notes.map((note) => {
-                return <NoteItem key={note.id} id={note.id} onDelete={onDelete} onArchive={onArchive} {...note} />;
+                // return <NoteItem key={note.id} id={note.id} onDelete={onDelete} onArchive={onArchive} createdAt={note.createdAt} {...note} />;
+                return <NoteItem key={note.id} id={note.id} onDelete={onDelete} onArchive={onArchive} onUnarchive={onUnarchive} createdAt={note.createdAt} {...note} />;
             })}
         </div>
-    )
+    );
 }
 
-export default NoteList
+NoteList.propTypes = {
+    notes: PropTypes.array.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onArchive: PropTypes.func.isRequired,
+    onUnarchive: PropTypes.func.isRequired
+};
+
+export default NoteList;
