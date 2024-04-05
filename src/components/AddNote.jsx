2dea4d8543b232fcb50f7e0c769ef2940965c484
SideeID <sandipratama164026@gmail.com>
2024-04-05 14:37:12 +0700

    Refactor AddNoteForm component in AddNote.jsx

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -7,38 +7,50 @@ function AddNoteForm({ onAdd }) {
     const navigate = useNavigate();
 
     const [newNote, setNewNote] = useState({
-        title: "",
-        body: "",
+        title: '',
+        body: '',
     });
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setNewNote({
             ...newNote,
-            [name]: value
+            [name]: value,
         });
     };
 
     const handleAdd = () => {
         onAdd(newNote);
         setNewNote({
-            title: "",
-            body: "",
+            title: '',
+            body: '',
         });
-        navigate('/')
+        navigate('/');
     };
 
     const remainingChars = maxLength - newNote.title.length;
 
     return (
-        <div className="add-note-form">
+        <div className='add-note-form'>
             <h2>Tambah catatan</h2>
-            <label htmlFor="title">Judul: </label>
-            <span>{remainingChars}/{maxLength}</span>
-            <input type="text" name="title" value={newNote.title} onChange={handleInputChange} maxLength={maxLength} />
-            <label htmlFor="body">Isi: </label>
-            <textarea name="body" value={newNote.body} onChange={handleInputChange} />
-            <div className="container-add-button">
+            <label htmlFor='title'>Judul: </label>
+            <span>
+                {remainingChars}/{maxLength}
+            </span>
+            <input
+                type='text'
+                name='title'
+                value={newNote.title}
+                onChange={handleInputChange}
+                maxLength={maxLength}
+            />
+            <label htmlFor='body'>Isi: </label>
+            <textarea
+                name='body'
+                value={newNote.body}
+                onChange={handleInputChange}
+            />
+            <div className='container-add-button'>
                 <button onClick={handleAdd}>Simpan</button>
             </div>
         </div>
@@ -46,7 +58,7 @@ function AddNoteForm({ onAdd }) {
 }
 
 AddNoteForm.propTypes = {
-    onAdd: PropTypes.func.isRequired
+    onAdd: PropTypes.func.isRequired,
 };
 
 export default AddNoteForm;
