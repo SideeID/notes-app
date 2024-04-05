8325768af9dbd1135da3971799c64dab59ca0446
SideeID <sandipratama164026@gmail.com>
2024-04-05 14:37:03 +0700

    Refactor import statements in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React from "react"
-import { createRoot } from "react-dom/client"
-import NoteApp from "./components/NoteApp"
-import { BrowserRouter } from "react-router-dom"
-import "./styles/style.css"
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import NoteApp from './components/NoteApp';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/style.css';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <NoteApp />
     </BrowserRouter>
-)
+);
